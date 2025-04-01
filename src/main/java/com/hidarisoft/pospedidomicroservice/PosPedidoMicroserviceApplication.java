2653d46c9f54fb4a03db0e6d94ba6eb7a3c4aeb6
maxpdr96 <maxpdr96@gmail.com>
2025-04-01 10:29:15 -0300

    Primeiro commit

---
+++ src/main/java/com/hidarisoft/pospedidomicroservice/PosPedidoMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hidarisoft.pospedidomicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosPedidoMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosPedidoMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hidarisoft/pospedidomicroservice/PosPedidoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidarisoft.pospedidomicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosPedidoMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
