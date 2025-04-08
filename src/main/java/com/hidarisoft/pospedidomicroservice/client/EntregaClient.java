4b81e4bad0a5c9e399e3c49fa42c71a5322dd36c
maxpdr96 <maxpdr96@gmail.com>
2025-04-08 19:07:04 -0300

    Add keycloak e geracao

--- src/main/java/com/hidarisoft/pospedidomicroservice/client/EntregaClient.java
+++ src/main/java/com/hidarisoft/pospedidomicroservice/client/EntregaClient.java
@@ -1,5 +1,6 @@
 package com.hidarisoft.pospedidomicroservice.client;
 
+import com.hidarisoft.pospedidomicroservice.config.FeignClientConfig;
 import com.hidarisoft.pospedidomicroservice.dto.CriacaoEntregaDTO;
 import com.hidarisoft.pospedidomicroservice.dto.EntregaResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -7,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "entregas-service", url = "${entregas.service.url}")
+@FeignClient(name = "entregas-service", url = "${entregas.service.url}", configuration = FeignClientConfig.class)
 public interface EntregaClient {
 
     @PostMapping("/entregas")
